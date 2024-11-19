279028ba1d79319e861ce29bb2d2e0fd0d71c491
Ko sungbin <kosb7544@gmail.com>
2024-11-19 13:58:26 +0900

    add initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into article(title, content) values('title 1', 'content 1')
+insert into article(title, content) values('title 2', 'content 2')
+insert into article(title, content) values('title 3', 'content 3')
