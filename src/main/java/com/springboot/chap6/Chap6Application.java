3dcff153d56130d7b8e46cc1e91f6384ec678e4f
Ko sungbin <kosb7544@gmail.com>
2024-11-19 10:59:10 +0900

    make project

---
+++ src/main/java/com/springboot/chap6/Chap6Application.java
@@ -0,0 +1,13 @@
+package com.springboot.chap6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/chap6/Chap6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.chap6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
