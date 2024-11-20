72d3d4493aa78a08b6e27bfa0bbc2ec76ee0d281
Ko sungbin <kosb7544@gmail.com>
2024-11-21 00:07:42 +0900

    add service, repo and entity of user

---
+++ src/main/java/com/springboot/chap6/domain/User.java
@@ -0,0 +1,75 @@
+package com.springboot.chap6.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Builder
+    public User(String email, String password, String auth) {
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    // check account expired
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    // check account locked
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    // check password expired
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    // check account usable
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/springboot/chap6/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.springboot.chap6.repository;
+
+import com.springboot.chap6.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
