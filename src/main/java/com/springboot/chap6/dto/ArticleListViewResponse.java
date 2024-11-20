9b8f64120234dfa5654ca12b922c77a8869fd485
Ko sungbin <kosb7544@gmail.com>
2024-11-20 15:38:14 +0900

    apply thymeleaf and add view page

---
+++ src/main/java/com/springboot/chap6/dto/ArticleListViewResponse.java
@@ -0,0 +1,17 @@
+package com.springboot.chap6.dto;
+
+import com.springboot.chap6.domain.Article;
+import lombok.Getter;
+
+@Getter
+public class ArticleListViewResponse {
+    private final Long id;
+    private final String title;
+    private final String content;
+
+    public ArticleListViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
