4b56bfb8d58e9845598502b15c120204df497039
Ko sungbin <kosb7544@gmail.com>
2024-11-22 12:58:11 +0900

    make table & repository for token

---
+++ src/main/java/com/springboot/chap6/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package com.springboot.chap6.repository;
+
+import com.springboot.chap6.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long id);
+    Optional<RefreshToken> findByRefreshToken(RefreshToken refreshToken);
+}
